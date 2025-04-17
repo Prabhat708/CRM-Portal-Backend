77739e9e73ab3436d053d6bd091973359a9dcc68
Prabhat Verma <117444677+Prabhat708@users.noreply.github.com>
2025-04-18 00:18:49 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const db = mysql.createPool({
   host: "localhost", 
   user: "root", 
-  password: "Prabhat@2003", 
+  password: "db password", 
   database: "crm_database",
   waitForConnections: true,
   connectionLimit: 10,
